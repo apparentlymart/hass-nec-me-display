9212ed1c24284ed23a8c7c36453d1d2a4df2dab5
Martin Atkins <mart@degeneration.co.uk>
2023-12-22 13:13:41 -0800

    Media player support

--- __init__.py
+++ __init__.py
@@ -20,7 +20,17 @@ from homeassistant.exceptions import ConfigEntryError, ConfigEntryNotReady
 
 from .const import CONF_CONTROLLER, CONF_MONITOR_ID, DOMAIN, LOGGER
 
-PLATFORMS = [Platform.REMOTE]
+PLATFORMS = [Platform.MEDIA_PLAYER, Platform.REMOTE]
+
+STANDARD_SOURCES: list[necme.InputTerminal] = [
+    necme.InputTerminal(0x11),
+    necme.InputTerminal(0x12),
+    necme.InputTerminal(0x88),
+    necme.InputTerminal(0x0F),
+]
+STANDARD_SOURCE_NAMES = {
+    t.builtin_name: t for t in STANDARD_SOURCES if t.builtin_name is not None
+}
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
@@ -55,6 +65,7 @@ class NECDisplayController:
     host: str
     monitor_id: int
     _lock: asyncio.Lock
+    _input_names = dict[necme.InputTerminal, str]
 
     def __init__(self, hass: HomeAssistant, host: str, monitor_id=int):
         """Initialize."""
@@ -64,6 +75,7 @@ class NECDisplayController:
         self._monitor = None
         self._user_count = 0
         self._lock = asyncio.Lock()
+        self._input_names = {}
 
     async def _async_init(self):
         LOGGER.info("async_init")
@@ -93,6 +105,44 @@ class NECDisplayController:
                 mode = await monitor.read_power_status()
         LOGGER.debug("monitor %d has status %r", self.monitor_id, mode)
         return mode
+
+    async def async_get_active_input_name(self) -> str:
+        """Return the name of the currently-selected input terminal."""
+        async with self._monitor_instance() as monitor, asyncio.timeout(3.0):
+            LOGGER.debug(
+                "requesting active input terminal for monitor %d", self.monitor_id
+            )
+            input = await monitor.read_active_input()
+            LOGGER.debug("monitor %d has active input %r", self.monitor_id, input)
+            name = self._input_names.get(input, None)
+            if name is not None:
+                LOGGER.debug(
+                    "monitor %d input %r has cached name: %s",
+                    self.monitor_id,
+                    input,
+                    name,
+                )
+                return name
+            LOGGER.debug(
+                "requesting name of input terminal %r from monitor %d",
+                input,
+                self.monitor_id,
+            )
+            name = (await monitor.read_input_name(input)).strip()
+            LOGGER.debug("monitor %d %r has name %s", self.monitor_id, input, name)
+            self._input_names[input] = name
+            return name
+
+    @property
+    def known_input_names(self) -> list:
+        """List of currently-known source names, often incomplete."""
+        return list(self._input_names.values())
+
+    def _input_terminal_by_name(self, name: str) -> necme.InputTerminal | None:
+        for k, v in self._input_names.items():
+            if v == name:
+                return k
+        return STANDARD_SOURCE_NAMES.get(name, None)
 
     @asynccontextmanager
     async def _monitor_instance(self):
---
+++ media_player.py
@@ -0,0 +1,97 @@
+"""A media player entity for an NEC M/ME-series display."""
+
+from __future__ import annotations
+
+import functools
+from typing import Any
+
+import necme
+
+from homeassistant.components.media_player import (
+    MediaPlayerDeviceClass,
+    MediaPlayerEntity,
+    MediaPlayerEntityFeature,
+    MediaPlayerState,
+    MediaType,
+)
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+
+from .const import DOMAIN
+from .entity import NECDisplayEntity
+
+SUPPORTED_FEATURES = (
+    MediaPlayerEntityFeature.TURN_ON | MediaPlayerEntityFeature.TURN_OFF
+)
+
+
+async def async_setup_entry(
+    hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
+) -> None:
+    """Set up the Samsung TV from a config entry."""
+    controller = hass.data[DOMAIN][entry.entry_id]
+    async_add_entities([NECDisplayPlayer(controller, entry)], update_before_add=True)
+
+
+class NECDisplayPlayer(NECDisplayEntity, MediaPlayerEntity):
+    """Media player entity type for NEC M/ME-series displays."""
+
+    _attr_has_entity_name = True
+    _attr_name = None
+    _attr_supported_features = SUPPORTED_FEATURES
+
+    async def async_update(self) -> None:
+        """Update the device properties."""
+        if self.hass.is_stopping:
+            return
+
+        await self._async_update_state()
+        await self._async_update_source()
+        self._attr_source_list = self._controller.known_input_names
+
+    async def _async_update_state(self) -> None:
+        tries = 0
+        while tries < 3:
+            try:
+                self._attr_state = _player_state(
+                    await self._controller.async_get_power_mode()
+                )
+                return
+            except TimeoutError:
+                tries = tries + 1
+
+        raise TimeoutError(
+            "failed to retrieve current power status after %d attempts" % tries
+        )
+
+    async def _async_update_source(self) -> None:
+        tries = 0
+        while tries < 3:
+            try:
+                self._attr_source = await self._controller.async_get_active_input_name()
+                return
+            except TimeoutError:
+                tries = tries + 1
+
+        raise TimeoutError(
+            "failed to retrieve current input terminal after %d attempts" % tries
+        )
+
+    async def async_turn_off(self, **kwargs: Any) -> None:
+        """Command the display to turn off."""
+        new_mode = await self._controller.async_turn_off()
+        self._attr_state = _player_state(new_mode)
+        self.async_schedule_update_ha_state()
+
+    async def async_turn_on(self, **kwargs: Any) -> None:
+        """Command the display to turn off."""
+        new_mode = await self._controller.async_turn_on()
+        self._attr_state = _player_state(new_mode)
+        self.async_schedule_update_ha_state()
+
+
+def _player_state(mode: necme.PowerMode) -> MediaPlayerState:
+    if mode is necme.PowerMode.ON:
+        return MediaPlayerState.ON
+    return MediaPlayerState.STANDBY
